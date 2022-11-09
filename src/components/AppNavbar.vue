b9d403de8b06083665f0e5d34f78859419e268f2
Yi-Ru Lan <a78945612385238@gmail.com>
2022-11-10 05:01:35 +0800

    Update ...

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,36 +1,87 @@
 <template>
   <ul>
-    <li v-for="(path, index) in paths" :key="index">
-      <RouterLink :to="path">{{ path }}</RouterLink>
+    <li>
+      <RouterLink to="/" v-slot="{ href }">{{ href }}</RouterLink>
+    </li>
+    <li>
+      <RouterLink :to="'/news'" v-slot="{ href }">{{ href }}</RouterLink>
+    </li>
+    <li>
+      <RouterLink :to="{ path: '/about' }" v-slot="{ href }">{{
+        href
+      }}</RouterLink>
+    </li>
+    <li>
+      <RouterLink :to="{ name: '/users/' }" v-slot="{ href }">{{
+        href
+      }}</RouterLink>
+    </li>
+    <li v-for="num in 2" :key="num">
+      <RouterLink
+        :to="{ name: '/users/[id]', params: { id: num } }"
+        v-slot="{ href }"
+        >{{ href }}</RouterLink
+      >
+    </li>
+    <li>
+      <RouterLink :to="{ path: '/users/3' }" v-slot="{ route }"
+        >/users/{{
+          (route as RouteLocationResolved<'/users/[id]'>).params.id
+        }}</RouterLink
+      >
+    </li>
+    <li>
+      <RouterLink :to="{ path: '/users/4/profile' }" v-slot="{ href }">{{
+        href
+      }}</RouterLink>
+    </li>
+    <li>
+      <RouterLink
+        :to="{ name: '/admin', query: { id: '4K6H' } }"
+        v-slot="{ href }"
+        >{{ href }}</RouterLink
+      >
     </li>
   </ul>
 </template>
 
 <script setup lang="ts">
-import { useLink } from 'vue-router/auto';
+import { type RouteLocationResolved, useLink } from 'vue-router/auto';
 
 const { isActive } = useLink({
-  to: { name: '/users/[id]', params: { id: 1 } },
+  to: '/users/3',
 });
-
 const { isExactActive } = useLink({
-  to: { path: '/users/2' },
+  to: { path: '/users' },
+});
+const { navigate } = useLink({
+  to: { name: '/users/[id]', params: { id: 6 } },
 });
 
-const { href, route } = useLink({
-  to: '/admin',
-});
+// import { type RouteLocationNormalizedLoaded, useLink } from 'vue-router/auto';
 
-const paths = [
-  '/',
-  '/news',
-  '/about',
-  '/users',
-  '/users/1',
-  '/users/2',
-  '/users/3/profile',
-  '/admin',
-];
+// const { isActive } = useLink({
+//   to: { name: '/users/[id]', params: { id: 1 } },
+// });
+
+// const { isExactActive } = useLink({
+//   to: { path: '/users/2' },
+// }).route.value.params.;
+
+// const { href, route } = useLink({
+//   to: '/admin',
+// });
+
+// const paths = [
+//   '/',
+//   '/news',
+//   '/about',
+//   '/users',
+//   '/users/1',
+//   '/users/2',
+//   '/users/3/profile',
+//   '/admin',
+// ];
 </script>
 
 <style scoped>
